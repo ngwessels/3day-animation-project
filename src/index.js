7a3af6ad8007ae140f36d5a94f24ed4ad955b60f
Nate Wessels <nwessels16@gmail.com>
2019-03-02 23:47:46 -0800

    add sass, dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-
 import './sass/styles.scss';
 console.log("hey!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
 
@@ -27,9 +30,29 @@ module.exports = {
             'css-loader',
             'sass-loader'
         ]
-      }
-    ]
+      },
 
+      {
+        test: /\.(gif|png|jpe?g)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/images/'
+              }
+            }
+          ]
+        },
+
+        {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+
+    ]
   },
 
 
@@ -37,8 +60,24 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: 'body',
       template: './src/index.html',
-      filename: 'index.html'
-      })
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/example.html',
+      filename: 'example.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+    new UglifyJsPlugin(),
+
+    new CleanWebpackPlugin(['dist'])
   ]
 
 };
